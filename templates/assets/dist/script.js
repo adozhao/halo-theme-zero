8ee6435805f0ae997c784b976557e9e50455b63b
zhaolu <zhaolu@yijiahe.com>
2024-01-31 15:16:47 +0800

    基础逻辑优化

--- templates/assets/dist/script.js
+++ templates/assets/dist/script.js
@@ -1,77 +1,77 @@
-window.main.currentColorScheme = localStorage.getItem('s') == 1 ? 'dark' : 'light'
+window.main.currentColorScheme = localStorage.getItem('theme-mode') === 'dark' ? 'dark' : 'light'
 window.onload = () => {
-    let d = document,
-        l = localStorage,
-        s = l.getItem('s'),
-        n = l.getItem('n'),
-        h = d.documentElement,
-        m = d.getElementById("dark-mode"),
-        b = () => {
-            h.dataset.colorScheme = 'light'
-            m.innerHTML = "dark"
-        },
-        c = () => {
-            h.dataset.colorScheme = 'dark'
-            m.innerHTML = "light"
-        },
-        o = d.getElementById("sans-font"),
-        e = () => {
-            h.dataset.fontScheme = 'serif'
-            o.innerHTML = "sans"
-        },
-        g = () => {
-            h.dataset.fontScheme = 'sans'
-            o.innerHTML = "serif"
-        };
+    let themeMode = localStorage.getItem('theme-mode')
+    let fontMode = localStorage.getItem('font-mode')
+    let html = document.documentElement
+    let themeModeEl = document.getElementById("dark-mode")
+    let fontModeEl = document.getElementById("sans-font")
 
     function changeCommentMode(mode) {
-        h.dataset.colorScheme = mode
-        const e = document.querySelectorAll("#comment div div");
-        e.length && e.forEach(n => {
-            let s;
-            const r = (s = n.shadowRoot) == null ? void 0 : s.querySelector(".halo-comment-widget");
-            r && (r.classList.remove('light', 'dark'), r.classList.add(mode))
+        html.dataset.colorScheme = mode
+        const shadowRootParent = document.querySelectorAll("#comment div div");
+        if(shadowRootParent.length){
+            shadowRootParent.forEach(parent => {
+                    let shadowRoot = parent.shadowRoot
+                    if(shadowRoot){
+                        const commentWidget = shadowRoot.querySelector('.halo-comment-widget')
+                        if(commentWidget){
+                            commentWidget.classList.remove('light', 'dark')
+                            commentWidget.classList.add(mode)
+                        }
+                    }
+                }
+            )
         }
-        )
     }
 
-    m.onclick = () => {
-        if (s == 2) {
+    themeModeEl.onclick = function(){
+        if (themeMode === 'light') {
             changeCommentMode('dark')
-            s = 1;
-            l.setItem('s', s);
-            c()
+            themeMode = 'dark'
+            localStorage.setItem('theme-mode', 'dark')
+            html.dataset.colorScheme = 'dark'
+            themeModeEl.innerText = "light"
         } else {
             changeCommentMode('light')
-            s = 2;
-            l.setItem('s', s);
-            b()
+            themeMode = 'light'
+            localStorage.setItem('theme-mode', 'light')
+            html.dataset.colorScheme = 'light'
+            themeModeEl.innerText = "dark"
         }
-    };
-    o.onclick = () => {
-        if (n == 2) {
-            n = 1;
-            l.setItem('n', n);
-            g()
+    }
+
+    fontModeEl.onclick = function(){
+        if (fontMode === 'serif') {
+            fontMode = 'sans'
+            localStorage.setItem('font-mode', 'sans');
+            html.dataset.fontScheme = 'sans'
+            fontModeEl.innerText = 'serif'
         } else {
-            n = 2;
-            l.setItem('n', n);
-            e()
+            fontMode = 'serif'
+            localStorage.setItem('font-mode', 'serif');
+            html.dataset.fontScheme = 'serif'
+            fontModeEl.innerText = "sans"
         }
-    };
-    if (!s) {
-        s = 2;
-        l.setItem('s', 2)
-    };
-    if (s == 1) {
-        c()
-    };
-    if (!n) {
-        n = 2;
-        l.setItem('n', 2)
-    };
-    if (n == 1) {
-        g()
-    };
+    }
+
+    if (!themeMode) {
+        themeMode = 'light'
+        localStorage.setItem('theme-mode', 'light')
+    }
+
+    if (themeMode === 'dark') {
+        html.dataset.colorScheme = 'dark'
+        themeModeEl.innerText = "light"
+    }
+
+    if (!fontMode) {
+        fontMode = 'serif'
+        localStorage.setItem('theme-mode', 'light')
+    }
+
+    if (fontMode === 'sans') {
+        html.dataset.fontScheme = 'sans'
+        fontModeEl.innerText = "serif"
+    }
 
 }
